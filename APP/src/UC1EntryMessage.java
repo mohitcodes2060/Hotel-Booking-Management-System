a13ed811e396cf22a3517098b08e569d6c4d66b2
mohitsolves2060 <v93.mohit@gmail.com>
2026-03-16 15:04:32 +0530

    UC1 completed

---
+++ APP/src/UC1EntryMessage.java
@@ -0,0 +1,6 @@
+public class UC1EntryMessage {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Booking Management System.");
+        System.out.println("System Initialized Successfully.");
+    }
+}
