bdd0e550833a744b655f4cf669cb508877be3a57
mohitsolves2060 <v93.mohit@gmail.com>
2026-03-16 16:04:41 +0530

    UC2 completed

---
+++ APP/src/SuiteRoom.java
@@ -0,0 +1,16 @@
+/**
+ * ==========================================================
+ * CLASS - SuiteRoom
+ * ==========================================================
+ *
+ * Represents a suite room type.
+ *
+ * @version 2.1
+ */
+
+public class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
