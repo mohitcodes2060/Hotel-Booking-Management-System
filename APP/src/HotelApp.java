108282fe3703f9614c491d9d4f5275530fb6b15c
mohitcodes2060 <v93.mohit@gmail.com>
2026-03-30 22:39:00 +0530

    UC8 completed

--- APP/src/HotelApp.java
+++ APP/src/HotelApp.java
@@ -1,38 +1,17 @@
-/**
- * ============================================================
- * MAIN CLASS - HotelApp
- * ============================================================
- *
- * Use Case 6: Room Allocation Processing
- *
- * @version 6.0
- */
 public class HotelApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Room Allocation Processing");
+        System.out.println("Booking History and Reporting\n");
 
-        // Step 1: Inventory
-        RoomInventory inventory = new RoomInventory();
+        BookingHistory history = new BookingHistory();
 
-        // Step 2: Booking Queue
-        BookingRequestQueue queue = new BookingRequestQueue();
+        // Directly add confirmed bookings (NO allocation print)
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
 
-        // Step 3: Add booking requests
-        queue.addRequest(new Reservation("Abhi", "Single"));
-        queue.addRequest(new Reservation("Subha", "Single"));
-        queue.addRequest(new Reservation("Vanmathi", "Suite"));
-
-        // Step 4: Allocation service
-        RoomAllocationService allocator = new RoomAllocationService();
-
-        // Step 5: Process queue (FIFO)
-        while (queue.hasPendingRequests()) {
-
-            Reservation r = queue.getNextRequest();
-
-            allocator.allocateRoom(r, inventory);
-        }
+        BookingReportService reportService = new BookingReportService();
+        reportService.generateReport(history);
     }
 }
